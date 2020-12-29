04629d669508db7c1f0cfadf56c0ef9f38b1e400
mic7x4 <mic7x4@gmail.com>
2020-12-29 14:02:55 +0200

    [feature]: getting the countries names

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useState } from 'react';
 import './App.css';
 
+// https://disease.sh/v3/covid-19/countries
+
 function App() {
+  const [countries,setContries] = useState([]);
+  useState(() => {
+    const getCountriesData = async ()=>{
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name:country.country,
+          value:country.countryInfo.iso2
+        }));
+        setContries(countries);
+      });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-     <h1>This is a covid 19 Tracker</h1>
+    <div className="app">
+     <div className="app__header">
+       <h1>COVID-19 TRACKER</h1>
+     <FormControl>
+    <Select variant="outlined" value="abc">
+      {countries.map((country)=>(
+         <MenuItem key={country.name} value={country.value}>{country.name}</MenuItem>
+      ))}
+    </Select>
+     </FormControl>
+     </div>
     </div>
   );
 }
-
+ 
 export default App;
